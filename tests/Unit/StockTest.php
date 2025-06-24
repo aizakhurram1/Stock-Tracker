27b7dd0945aa00f8213d5923e471d50437f22aa9
aizakhurram1 <aiza.khurram@dubizzlelabs.com>
2025-06-24 10:45:54 +0500

    Fake client implemented

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Unit;
 
 use App\Clients\ClientsException;
+use App\Clients\StockStatus;
 use App\Models\Retailer;
 use App\Models\Stock;
 use Database\Seeders\RetailerWithProduct;
+use Facades\App\Clients\ClientFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase; // ✅ use Laravel's base test case
 
@@ -25,5 +27,22 @@ class StockTest extends TestCase
         $this->expectException(ClientsException::class);
 
         Stock::first()->track();
+    }
+
+    /** @test */
+    public function it_updates_loal_stock_status_after_being_tracked()
+    {
+
+        $this->seed(RetailerWithProduct::class);
+
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(
+            new StockStatus($available = true, $price = 9900)
+        );
+
+        $stock = tap(Stock::first())->track();
+
+        $this->assertTrue($stock->in_stock);
+        $this->assertEquals(9900, $stock->price);
+
     }
 }
